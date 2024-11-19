c846e61a661ace318ee1226b2c59dffe1916dc0f
Klee Collage <klee.collage@gmail.com>
2024-11-18 20:31:23 -0600

    Endpoints tests
    tokens and refresh tokens generated

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,70 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-btn @click="access">Ingresar</q-btn>
+  <q-btn @click="createLink">Crear Link</q-btn>
+  {{ token }} - {{ expireIn }}
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script setup>
+import { api } from 'src/boot/axios';
+import { ref } from 'vue';
+// CONSTANTS
+const token = ref('')
+const expireIn = ref('')
 
-export default defineComponent({
-  name: 'IndexPage'
-});
+// METHODS
+const access = async () => {
+  try {
+    console.log('clickity click')
+    const res = await api.post('/auth/login', {
+      email: "jane.smith@mail.com",
+      password: "123456"
+    });
+    token.value = res.data.token;
+    expireIn.value = res.data.expiresIn;
+    setTime();
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+const createLink = async () => {
+  try {
+    const res = await api({
+      method: 'POST',
+      url: '/links',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token.value,
+      },
+      data: {
+        longLink: "pornhub.com"
+      }
+    });
+    console.log(res.data);
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+const setTime = () => {
+  setTimeout(() => {
+    console.log("token expired , new token created")
+    refreshToken();
+  }, expireIn.value * 1000 - 7000);
+}
+
+
+const refreshToken = async () => {
+  try {
+    const res = await api.get('/auth/refresh')
+    token.value = res.data.token;
+    expireIn.value = res.data.expiresIn;
+    setTime();
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+// CALLS
+refreshToken();
 </script>
